6996a696feceba35d29ed82fa48a5c18d9ae398b
Lucky <lokendramalhotra2001@gmail.com>
2024-03-14 23:40:40 +0530

    Done

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+          <div className="App">
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+            </Routes>
+          </div>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
